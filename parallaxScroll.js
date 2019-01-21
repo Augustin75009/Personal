2487b48b0f6200d463cddf539da6f40cab6902f6
augustin cousin <augustincousin.pro@gmail.com>
2019-01-21 21:59:07 +0100

    fixed offset issue

--- parallaxScroll.js
+++ parallaxScroll.js
@@ -1,28 +1,13 @@
 const parallaxScroll = document.querySelector(".name")
-const parallaxScrollFirst = document.querySelector(".first-card")
 const parallaxScrollBis = document.querySelector(".astro")
 const parallaxScrollBisBis = document.querySelector(".astro2")
-// const height = window.top.innerHeight;
 
 window.addEventListener("scroll", function() {
   let offset = window.pageYOffset;
+  let offsetBis = window.pageYOffset - height;
   parallaxScroll.style.backgroundPositionY = offset * 0.8 + "px";
   parallaxScroll.style.opacity = 1 - (offset / 400);
-  parallaxScrollBisBis.style.backgroundPositionY = (offset + height - 465) * 0.65 + "px"
-  // console.log(offset)
-})
-
-
-
-// window.addEventListener("scroll", function() {
-//   let offset = window.pageYOffset;
-//   // console.log("Offset:" + offset * 0.7);
-//   parallaxScroll.style.PositionY = offset * 0.7 + "px";
-// })
-
-
-  window.addEventListener("scroll", function() {
-  let offsetBis = window.pageYOffset - height;
-  console.log(offsetBis);
-  parallaxScrollBis.style.backgroundPositionY = offsetBis * 0.65 + "px";
+  parallaxScrollBisBis.style.backgroundPositionY = offsetBis * 0.65 + height + "px";
+  parallaxScrollBis.style.backgroundPositionY = offsetBis * 0.65  + "px";
+  console.log(height)
 })
