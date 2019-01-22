279c706308e13129e952e9b7a2acc96bf1d10da7
augustin cousin <augustincousin.pro@gmail.com>
2019-01-22 12:26:29 +0100

    welcome text

--- parallaxScroll.js
+++ parallaxScroll.js
@@ -1,12 +1,14 @@
 const parallaxScroll = document.querySelector(".name")
 const parallaxScrollBis = document.querySelector(".astro")
 const parallaxScrollBisBis = document.querySelector(".astro2")
+const parallaxScrollBulle = document.querySelector(".bulle")
 
 window.addEventListener("scroll", function() {
   let offset = window.pageYOffset;
   let offsetBis = window.pageYOffset - height;
   parallaxScroll.style.backgroundPositionY = offset * 0.8 + "px";
   parallaxScroll.style.opacity = 1 - (offset / 400);
+  parallaxScrollBulle.style.visibility = "hidden";
   parallaxScrollBisBis.style.backgroundPositionY = offsetBis * 0.65 + height + "px";
   parallaxScrollBis.style.backgroundPositionY = offsetBis * 0.65  + "px";
   console.log(height)
