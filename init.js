bcb0daaffb549a48b7b8cad72abae03ba94f3a48
augustin cousin <augustincousin.pro@gmail.com>
2019-03-07 17:24:49 +0100

    phone style

--- init.js
+++ init.js
@@ -4,5 +4,3 @@ var parallax = new Parallax(scene);
 var sceneSkills = document.getElementById('scene-skills');
 var parallax = new Parallax(sceneSkills);
 
-var sceneSkills = document.getElementById('scene-description');
-var parallax = new Parallax(sceneSkills);
